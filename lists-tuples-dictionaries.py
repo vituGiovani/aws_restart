a5016404c7794fc1da51b9c4c3af607eeb02903e
vituGiovani <victorgiovanni769@gmail.com>
2025-11-19 16:48:13 -0300

    Add files via upload

---
+++ lists-tuples-dictionaries.py
@@ -0,0 +1,34 @@
+print('------ Lists ------')
+
+myFruitList = ["apple", "banana", "cherry"]
+print(myFruitList)
+print(type(myFruitList))
+
+print(myFruitList[0])
+print(myFruitList[1])
+print(myFruitList[2])
+
+myFruitList[2] = "orange"
+print(myFruitList)
+
+print('------ Tuples ------')
+myFinalAnswerTuple = ("apple", "banana", "pineapple")
+print(myFinalAnswerTuple)
+print(type(myFinalAnswerTuple))
+
+print(myFinalAnswerTuple[0])
+print(myFinalAnswerTuple[1])
+print(myFinalAnswerTuple[2])
+
+print('------ Dicitionaries ------')
+myFavoriteFruitDictionary = {
+  "Akua" : "apple",
+  "Saanvi" : "banana",
+  "Paulo" : "pineapple"
+}
+print(myFavoriteFruitDictionary)
+print(type(myFavoriteFruitDictionary))
+
+print(myFavoriteFruitDictionary["Akua"])
+print(myFavoriteFruitDictionary["Saanvi"])
+print(myFavoriteFruitDictionary["Paulo"])
